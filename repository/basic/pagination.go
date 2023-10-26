56b871847b3198d413e37dfdff8ad68e88d0bbaf
Jins <jinsiyuan@58ganji.com>
2023-10-26 17:47:30 +0800

    分页

--- repository/basic/pagination.go
+++ repository/basic/pagination.go
@@ -2,6 +2,13 @@ package basic
 
 // Pagination 分页
 type Pagination struct {
-	Page     int32 `json:"page" form:"page"`
-	PageSize int32 `json:"page_size" form:"page_size"`
+	Page     int `json:"page" form:"page"`
+	PageSize int `json:"page_size" form:"page_size"`
+}
+
+func (p *Pagination) Offset() int {
+	if p.Page == 0 {
+		p.Page = 1
+	}
+	return (p.Page - 1) * p.PageSize
 }
